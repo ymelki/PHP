893717aa7c1161d4feecc68bc1152e4f7d89f033
ymelki <67371192+ymelki@users.noreply.github.com>
2023-11-08 15:09:42 +0100

    create

---
+++ createbook.php
@@ -0,0 +1,13 @@
+<form  action="savebook.php" method="post">
+    <div>
+      <label for="title">Title :</label>
+      <input type="text"  id="name"  name="book_title">
+    </div> 
+    <div>
+      <label  for="description">Description :</label>
+      <textarea  id="message"  name="book_description"></textarea>
+    </div>
+    <div  class="button">
+      <button  type="submit">Send</button>
+    </div>
+  </form>
---
+++ savebook.php
@@ -0,0 +1,9 @@
+<?php
+// 1. get data from form
+var_dump($_POST);
+
+// 2. connect to B.D.
+
+// 3. save data in B.D (insert into)
+
+// retirect to books.php
