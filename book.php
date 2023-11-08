c115071c0bd79aa88d482fc96c67dfabbd1bb9ff
ymelki <67371192+ymelki@users.noreply.github.com>
2023-11-08 09:53:15 +0100

    add detail

--- book.php
+++ book.php
@@ -9,4 +9,33 @@ book.php?id=3&name=yoel&lastname="melki"
 */
 var_dump($_GET);
 echo $_GET['id'];
+// $id contains id from url
+$id=$_GET['id'];
+
+// 1 connect to D.B.
+$pdo = new \PDO('mysql:host=localhost;dbname=book', 'root', '');
+
+// Query : SELECT * FROM book where id=$id;
+/*
+$query = "SELECT * FROM book where id=$id";
+$statement = $pdo->query($query);
+$book = $statement->fetch(PDO::FETCH_ASSOC);
+*/
+
+// query with id 
+$query = "SELECT * FROM book where id=:myId";
+// query prepare with PDO 
+$statement = $pdo->prepare($query);
+// definie ":myid"
+$statement->bindValue(':myId', $id, \PDO::PARAM_INT);
+// execute
+$statement->execute();
+// get data 
+$book = $statement->fetch(PDO::FETCH_ASSOC);
+
+var_dump($book);
+
+echo "ID : " . $book['id'] . "<br>".
+     "Title :  " . $book['title'] . "<br>".
+     "Description :  " . $book['description'] . "<br>";
 ?>
